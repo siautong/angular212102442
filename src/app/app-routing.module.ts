c19662aa27ced6eb9bdc978bdcca2e91f1b2fc99
siautong <siautong2003@gmail.com>
2023-10-03 19:59:15 +0700

    Project Terakhir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+{ path: "dashboard", component: DashboardComponent },
+{ path: "login", component: LoginComponent },
+{ path: "", redirectTo: "login", pathMatch: "full" }
+]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot (routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
