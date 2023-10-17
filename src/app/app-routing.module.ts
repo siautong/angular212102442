8b66b0cdc55e011267a60cf927b97afcbcbe97c7
siautong <siautong2003@gmail.com>
2023-10-17 19:28:42 +0700

    update dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
 const routes: Routes = [
 { path: "admin", component: AdminComponent },
 { path: "login", component: LoginComponent },
 { path: "register", component: RegisterComponent },
+{ path: "dashboard", component: DashboardComponent },
+{ path: "dashboard2", component: Dashboard2Component },
+{ path: "dashboard3", component: Dashboard3Component },
 { path: "", redirectTo: "login", pathMatch: "full" }
 ]
 @NgModule({
